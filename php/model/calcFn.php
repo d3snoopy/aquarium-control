0df09c878b7d33e04762e887e7ad1aeb9b76c24d
Stuart Asp <d3snoopy@gmail.com>
2017-10-06 21:54:34 -0400

    Adding new files

---
+++ php/model/calcFn.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+Copyright 2017 Stuart Asp: d3snoopy AT gmail
+
+This file is part of Aqctrl.
+
+Aqctrl is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Aqctrl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aqctrl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// calcFn.php
+
+set_include_path("template:model");
+
+include 'functionFn.php';
+
+// These are supporting functions to do the calculation heavy lifting.
+// The functions all expect to be handed data relevant to the calculations (I.E. they don't
+// do the queries for you, but given the data as perscribed it will do the necessary calculations.
+// They will accept bulk data and filter, assuming the data is appropriately pre-sorted as parscrribed.
+
+function sourceCalc()
+{
+  //Data Inputs
+
+
+  //Return format
+
+}
---
+++ php/template/newHost.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+Copyright 2017 Stuart Asp: d3snoopy AT gmail
+
+This file is part of Aqctrl.
+
+Aqctrl is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Aqctrl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aqctrl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+configFn.php
+
+
+Functions to support the configure.php page.
+
+configForm - function to generate the form to fill out
+
+configRtn - function to handle the return a POST of SetupForm
+
+*/
+
+// newHosts.php
+
+// This returns a link when a new host is found.
+
+set_include_path("../template:../model");
+
+include 'model.php';
+
+//Test the db connection and connect
+$mysqli = \aqctrl\db_connect();
+
+if(!$mysqli) return;
+
+$res = mysqli_query($mysqli, "SELECT id FROM host WHERE auth = '0'");
+
+if(mysqli_num_rows($res)) {
+  echo "<p class='alignleft'>\n";
+  echo(mysqli_num_rows($res) . " new hosts connected, click <a href='./setup.php'>here</a> to configure!\n");
+  echo "</p>\n";
+}
+
+?>
+
+<p class="alignright">Created by: Stuart Asp</p>
+<div style="clear:both;"></div>
